6aca6aba2f242cbbc67dc31612630a7c5793910d
João Gabriel <joao.florv2005@gmail.com>
2024-11-02 22:01:38 -0300

    Criando opcao 06

---
+++ Opcao6.Designer.cs
@@ -0,0 +1,383 @@
+﻿namespace AtividadeAvaliativaGaal
+{
+    partial class Opcao6
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtEquacaoC1 = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.txtEquacaoZ1 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtEquacaoB1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtEquacaoY1 = new System.Windows.Forms.TextBox();
+            this.txtEquacaoA1 = new System.Windows.Forms.TextBox();
+            this.txtEquacaoX1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtEquacaoC2 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtEquacaoZ2 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtEquacaoB2 = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.txtEquacaoY2 = new System.Windows.Forms.TextBox();
+            this.txtEquacaoA2 = new System.Windows.Forms.TextBox();
+            this.txtEquacaoX2 = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.lblResposta = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.txtEquacaoC1);
+            this.groupBox1.Controls.Add(this.label10);
+            this.groupBox1.Controls.Add(this.txtEquacaoZ1);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.txtEquacaoB1);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtEquacaoY1);
+            this.groupBox1.Controls.Add(this.txtEquacaoA1);
+            this.groupBox1.Controls.Add(this.txtEquacaoX1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(36, 46);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(313, 175);
+            this.groupBox1.TabIndex = 32;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Equações paramétricas da reta 01:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(163, 125);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(23, 18);
+            this.label9.TabIndex = 25;
+            this.label9.Text = "C:";
+            // 
+            // txtEquacaoC1
+            // 
+            this.txtEquacaoC1.Location = new System.Drawing.Point(190, 122);
+            this.txtEquacaoC1.Name = "txtEquacaoC1";
+            this.txtEquacaoC1.Size = new System.Drawing.Size(100, 24);
+            this.txtEquacaoC1.TabIndex = 24;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(6, 125);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(29, 18);
+            this.label10.TabIndex = 23;
+            this.label10.Text = "Z1:";
+            // 
+            // txtEquacaoZ1
+            // 
+            this.txtEquacaoZ1.Location = new System.Drawing.Point(36, 122);
+            this.txtEquacaoZ1.Name = "txtEquacaoZ1";
+            this.txtEquacaoZ1.Size = new System.Drawing.Size(100, 24);
+            this.txtEquacaoZ1.TabIndex = 22;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(163, 83);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(22, 18);
+            this.label8.TabIndex = 21;
+            this.label8.Text = "B:";
+            // 
+            // txtEquacaoB1
+            // 
+            this.txtEquacaoB1.Location = new System.Drawing.Point(190, 80);
+            this.txtEquacaoB1.Name = "txtEquacaoB1";
+            this.txtEquacaoB1.Size = new System.Drawing.Size(100, 24);
+            this.txtEquacaoB1.TabIndex = 20;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 83);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 18);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "Y1:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(163, 39);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(21, 18);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "A:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 18);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "X1:";
+            // 
+            // txtEquacaoY1
+            // 
+            this.txtEquacaoY1.Location = new System.Drawing.Point(36, 80);
+            this.txtEquacaoY1.Name = "txtEquacaoY1";
+            this.txtEquacaoY1.Size = new System.Drawing.Size(100, 24);
+            this.txtEquacaoY1.TabIndex = 16;
+            // 
+            // txtEquacaoA1
+            // 
+            this.txtEquacaoA1.Location = new System.Drawing.Point(190, 36);
+            this.txtEquacaoA1.Name = "txtEquacaoA1";
+            this.txtEquacaoA1.Size = new System.Drawing.Size(100, 24);
+            this.txtEquacaoA1.TabIndex = 15;
+            // 
+            // txtEquacaoX1
+            // 
+            this.txtEquacaoX1.Location = new System.Drawing.Point(36, 36);
+            this.txtEquacaoX1.Name = "txtEquacaoX1";
+            this.txtEquacaoX1.Size = new System.Drawing.Size(100, 24);
+            this.txtEquacaoX1.TabIndex = 14;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(243, 25);
+            this.label1.TabIndex = 33;
+            this.label1.Text = "Distância entre duas retas:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.txtEquacaoC2);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.txtEquacaoZ2);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.txtEquacaoB2);
+            this.groupBox2.Controls.Add(this.label11);
+            this.groupBox2.Controls.Add(this.label12);
+            this.groupBox2.Controls.Add(this.label13);
+            this.groupBox2.Controls.Add(this.txtEquacaoY2);
+            this.groupBox2.Controls.Add(this.txtEquacaoA2);
+            this.groupBox2.Controls.Add(this.txtEquacaoX2);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(412, 46);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(313, 175);
+            this.groupBox2.TabIndex = 33;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Equações paramétricas da reta 02:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(163, 125);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(23, 18);
+            this.label5.TabIndex = 25;
+            this.label5.Text = "C:";
+            // 
+            // txtEquacaoC2
+            // 
+            this.txtEquacaoC2.Location = new System.Drawing.Point(190, 122);
+            this.txtEquacaoC2.Name = "txtEquacaoC2";
+            this.txtEquacaoC2.Size = new System.Drawing.Size(100, 24);
+            this.txtEquacaoC2.TabIndex = 24;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(6, 125);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(29, 18);
+            this.label6.TabIndex = 23;
+            this.label6.Text = "Z1:";
+            // 
+            // txtEquacaoZ2
+            // 
+            this.txtEquacaoZ2.Location = new System.Drawing.Point(36, 122);
+            this.txtEquacaoZ2.Name = "txtEquacaoZ2";
+            this.txtEquacaoZ2.Size = new System.Drawing.Size(100, 24);
+            this.txtEquacaoZ2.TabIndex = 22;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(163, 83);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(22, 18);
+            this.label7.TabIndex = 21;
+            this.label7.Text = "B:";
+            // 
+            // txtEquacaoB2
+            // 
+            this.txtEquacaoB2.Location = new System.Drawing.Point(190, 80);
+            this.txtEquacaoB2.Name = "txtEquacaoB2";
+            this.txtEquacaoB2.Size = new System.Drawing.Size(100, 24);
+            this.txtEquacaoB2.TabIndex = 20;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(6, 83);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(29, 18);
+            this.label11.TabIndex = 19;
+            this.label11.Text = "Y1:";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(163, 39);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(21, 18);
+            this.label12.TabIndex = 18;
+            this.label12.Text = "A:";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(6, 39);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(30, 18);
+            this.label13.TabIndex = 17;
+            this.label13.Text = "X1:";
+            // 
+            // txtEquacaoY2
+            // 
+            this.txtEquacaoY2.Location = new System.Drawing.Point(36, 80);
+            this.txtEquacaoY2.Name = "txtEquacaoY2";
+            this.txtEquacaoY2.Size = new System.Drawing.Size(100, 24);
+            this.txtEquacaoY2.TabIndex = 16;
+            // 
+            // txtEquacaoA2
+            // 
+            this.txtEquacaoA2.Location = new System.Drawing.Point(190, 36);
+            this.txtEquacaoA2.Name = "txtEquacaoA2";
+            this.txtEquacaoA2.Size = new System.Drawing.Size(100, 24);
+            this.txtEquacaoA2.TabIndex = 15;
+            // 
+            // txtEquacaoX2
+            // 
+            this.txtEquacaoX2.Location = new System.Drawing.Point(36, 36);
+            this.txtEquacaoX2.Name = "txtEquacaoX2";
+            this.txtEquacaoX2.Size = new System.Drawing.Size(100, 24);
+            this.txtEquacaoX2.TabIndex = 14;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(36, 237);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(689, 40);
+            this.btnCalcular.TabIndex = 34;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // lblResposta
+            // 
+            this.lblResposta.AutoSize = true;
+            this.lblResposta.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResposta.Location = new System.Drawing.Point(32, 309);
+            this.lblResposta.Name = "lblResposta";
+            this.lblResposta.Size = new System.Drawing.Size(0, 20);
+            this.lblResposta.TabIndex = 35;
+            // 
+            // Opcao6
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(759, 450);
+            this.Controls.Add(this.lblResposta);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Opcao6";
+            this.Text = "Opcao6";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtEquacaoC1;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox txtEquacaoZ1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtEquacaoB1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtEquacaoY1;
+        private System.Windows.Forms.TextBox txtEquacaoA1;
+        private System.Windows.Forms.TextBox txtEquacaoX1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtEquacaoC2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtEquacaoZ2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtEquacaoB2;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.TextBox txtEquacaoY2;
+        private System.Windows.Forms.TextBox txtEquacaoA2;
+        private System.Windows.Forms.TextBox txtEquacaoX2;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label lblResposta;
+    }
+}
---
+++ Opcao6.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AtividadeAvaliativaGaal
+{
+    public partial class Opcao6 : Form
+    {
+        public Opcao6()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            // FAZ AQUI, A LBL QUE VC VAI COLOCAR O RESULTADO É lblResposta
+        }
+    }
+}
